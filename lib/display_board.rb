3ff60569cfde7e4f2f737cc47ba1ceecdee94d84
J Lin <joseph@josephchilin.com>
2020-02-01 18:17:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def empty_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+
 def display_board()
 
 end
