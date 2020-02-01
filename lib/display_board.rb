553d0791bcf0f4b2a4cfb2020cc7a966db342c7a
J Lin <joseph@josephchilin.com>
2020-02-01 18:21:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,6 +9,10 @@ def empty_board
 end
 
 
-def display_board()
-
+def display_board(board)
+  puts " board[0] | board [1] | board [2]"
+  puts "-----------"
+  puts " board[3] | board [4] | board [5]"  
+  puts "-----------"
+  puts " board[7] | board [8] | board [9]"
 end
